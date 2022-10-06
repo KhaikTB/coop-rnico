e612cb08fe251455441962e4f58b69e09c53af0f
Tiago <78740896+KhaikTB@users.noreply.github.com>
2022-10-06 03:38:40 +0100

    new update

--- script.js
+++ script.js
@@ -1,17 +1,69 @@
-// document.getElementById("btn1").onclick = function() {btn1Click()}
-// document.getElementById("btn2").onclick = function() {btn2Click()}
+// Saber mais Section
+const readMoreContent = document.querySelector('.read-more-container')
 
-// function btn1Click()
+readMoreContent.addEventListener('click', event => 
+{
+  const current = event.target;
+  const ReadMoreBtn = current.className.includes('read-more-btn')
+
+  if(!ReadMoreBtn) return
+
+  const currentText = event.target.parentNode.querySelector('.read-more-text')
+
+  currentText.classList.toggle('read-more-text--show')
+
+  current.textContent = current.textContent.includes('Saber mais') ? 
+  "Saber menos..." : "Saber mais..."
+})
+// ...
+
+// Pedir Contacto Btn Section
+// const openContactBtn = document.querySelectorAll('[data-modal-target]')
+// const closeContactBtn = document.querySelectorAll('[data-close-button]')
+// const overlay = document.getElementById('overlay2')
+
+// openContactBtn.forEach(button => 
 // {
-//     document.getElementById("btn1").innerHTML = "Clicked"
+//   button.addEventListener('click', () => 
+//   {
+//     const modal = document.querySelector(button.dataset.modalTarget)
+//     openModal(modal)
+//   })
+// })
+
+// closeContactBtn.forEach(button => 
+// {
+//   button.addEventListener('click', () => 
+//   {
+//     const modal = button.closest('.modal')
+//     closeModal(modal)
+//   })
+// })
+
+// overlay.addEventListener('click', () => 
+// {
+//   const modals = document.querySelectorAll('.modal.active')
+//   modals.forEach(modal => 
+//   {
+//     closeModal(modal)
+//   })
+// })
+
+// function openModal(modal)
+// {
+//   if (modal == null) return
+//   modal.classList.add('active')
+//   overlay.classList.add('active')
 // }
 
-// function btn2Click()
+// function closeModal(modal)
 // {
-//     document.getElementById("btn2").innerHTML = "Clicked"
+//   if (modal == null) return
+//   modal.classList.remove('active')
+//   overlay.classList.remove('active')
 // }
+// ...
 
-// ................................................................................................
 
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
