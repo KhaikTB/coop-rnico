7504c3d1e9b407c77bd9dd7d1437b5ae888b9f3c
Tiago <78740896+KhaikTB@users.noreply.github.com>
2022-11-28 03:52:54 +0000

    new commit

--- script.js
+++ script.js
@@ -112,24 +112,53 @@ function closeModal2(modal2)
 const multiStepForm = document.querySelector("[data-multi-step")
 const formSteps = [...multiStepForm.querySelectorAll("[data-step]")]
 
-let currentStep = formSteps.findIndex(step => {
+let currentStep = formSteps.findIndex(step => 
+{
   return step.classList.contains("active")
 })
 
 if (currentStep < 0)
 {
   currentStep = 0
-  formSteps[currentStep].classList.add("active")
+  showCurrentStep()
 }
 
-multiStepForm.addEventListener("click", e => {
-  if (e.target.matches("[data-next"))
+multiStepForm.addEventListener("click", e => 
+{
+  let incrementor
+  if (e.target.matches("[data-next]"))
   {
-    currentStep += 1
+    incrementor = 1
   }
-  else if
-    (e.target.matches("[data-previous]")) {
-      currentStep -= 1
-    }
-  console.log(currentStep)
+  else if (e.target.matches("[data-previous]"))
+  {
+    incrementor = -1
+  }
+
+  if (incrementor == null) return
+
+  const inputs = [...formSteps[currentStep].querySelectorAll("input")]
+  const allValid = inputs.every(input => input.reportValidity())
+  if (allValid)
+  {
+    currentStep += incrementor
+    showCurrentStep()
+  }
 })
+
+formSteps.forEach(step => 
+{
+  step.addEventListener("animationend", e => 
+  {
+    formSteps[currentStep].classList.remove("hide")
+    e.target.classList.toggle("hide", !e.target.classList.contains("active"))
+  })
+})
+
+function showCurrentStep() 
+{
+  formSteps.forEach((step, index) => 
+  {
+    step.classList.toggle("active", index === currentStep)
+  })
+}
